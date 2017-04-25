eef0d017b6c57115fb336da296a21e063098444b
joemetha <icloud.joe@gmail.com>
2017-04-25 13:16:08 +0700

    template for ble

---
+++ app/lukana/map/lukana.map.component.ts
@@ -0,0 +1,11 @@
+
+
+import {Component} from "@angular/core";
+@Component({
+  selector: 'my-map',
+  templateUrl: 'app/lukana/map/lukana.map.component.html'
+})
+export class LukanaMapComponent {
+
+
+}
